79be1ec66677de7a84eedfd02a556f8717342bb8
Shimon Yzhaki <mct1957mct@gmail.com>
2023-02-04 21:45:20 +0000

    V0.01

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -18,48 +18,108 @@ sap.ui.define([
                 var oModel = new JSONModel(Device);
                 oModel.setDefaultBindingMode("OneWay");
                 return oModel;
-        },
+            },
 
-        viewModel: function(sViewName) {
-            let oDefault = {};
+            getText: function (sKey, aParameters=[]) {
+                this.getView().getModel("i18n").getResourceBundle().getText(sKey, aParameters);
+            },
 
-            switch (sViewName) {
-                case "main":
-                    oDefault    = {
-                        busy: true,
-                        chart01: {
-                            title:{ 
-                                text:       'SLA לידים בחריגה'
+            viewModel: function(sViewName) {
+                let oDefault = {};
+
+                switch (sViewName) {
+                    case "main":
+                        oDefault    = {
+                            busy: true,
+                            date:   new Date(),
+                            selectedBrand: "",
+                            selectedTeam: "1",
+                            comboBoxTeam: [ { key: "1", text: "Display as me"},
+                                            { key: "2", text: "Display as Teams"}],
+                            chart01: {
+                                title:{ 
+                                    text:       "Chart 01"//this.getText("SLA_EXCEPTIONS")
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                },
+                                categoryAxis: {
+                                    axisLine: {
+                                        visible: false
+                                    }
+                                }
                             },
-                            dataLabel:{
-                                visible:    true,
-                                showTotal:  false
+                            chart02: {
+                                title:{ 
+                                    text: 'Test 01'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  true}, 
+                                categoryAxis:{
+                                    title: {visible: false}
+                                },
+                                plotArea: { 
+                                    colorPalette: ['green', 'red', '#711e82', '#40c7a1', '#969696'] 
+                                }
+                            },
+                            chart03: {
+                                title:{ 
+                                    text:       'Test 03'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                }
+                            },
+                            chart04: {
+                                title:{ 
+                                    text:       'Test 04'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                }
+                            },
+                            chart05: {
+                                title:{ 
+                                    text:       'Test 05'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                }
+                            },
+                            chart06: {
+                                title:{ 
+                                    text:       'Test 06'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                }
+                            },
+                            chart07: {
+                                title:{ 
+                                    text:       'Test 07'
+                                },
+                                dataLabel:{
+                                    visible:    true,
+                                    showTotal:  false
+                                }
                             }
-                        },
-                        chart02: {
-                            title:{ 
-                                text: 'Test 01'
-                            },
-                            dataLabel:{
-                                visible:    true,
-                                showTotal:  true}, 
-                            categoryAxis:{
-                                title: {visible: false}
-                            },
-                            plotArea: { 
-                                colorPalette: ['green', 'red', '#711e82', '#40c7a1', '#969696'] 
-                            }
-                        }
-                    };
+                            
+                        };
 
-                    break;
-            
-                default:
-                    break;
+                        break;
+                
+                    default:
+                        break;
+                }
+
+                return new JSONModel(oDefault);    
             }
-
-            return new JSONModel(oDefault);    
-        }
 
     };
 });
